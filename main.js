27e083e516bb538d1e117c4b5cdefa3eba7c7e7d
Bai-was-taken <71526440+Bai-was-taken@users.noreply.github.com>
2021-01-22 16:16:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ function draw(){
     noFill();
     stroke(255);
     strokeWeight(3);
-    rect(player.x,player.y,playerImage.width,playerImage.height);
     rect(10,10,100,20);
     fill(0,255,0);
     strokeWeight(0);
@@ -63,7 +62,7 @@ function draw(){
        }
         stroke(255);
         strokeWeight(3);
-        rect(meteor.x,meteor.y,48,48);
+//        rect(meteor.x,meteor.y,48,48);
         meteor.y -= -5
 
    });
